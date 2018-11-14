ab55b460385cb7297bbdea4e061b442d1f81af64
Ricardogll <rikilonga@gmail.com>
2018-11-14 12:42:18 +0100

    Adding bar

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -147,5 +148,10 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void onClickUsers(View view) {
+
+
     }
 }
